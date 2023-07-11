2c6e921cfd9d48e9679b883466d1f86305246848
alexbarker234 <alex.barker234@gmail.com>
2023-07-11 19:27:03 +0800

    snowfall is a canvas now

--- snowbound/script.js
+++ snowbound/script.js
@@ -1,21 +1,28 @@
 spin = 0;
 spinSpeed = 0;
 frost = 0;
+
+autoClickers = [];
+
+snowfallCanvas = undefined;
+
 $( document ).ready(function() {
     frost = parseInt(localStorage.getItem('frost'));
     frost = isNaN(frost) ? 0 : frost;
     $('#frost').find('span').html(frost)
-
+    snowfallCanvas = $("#snowfall").get(0)
 
     const snowflake = $('.snowflake');
     snowflake.on('click', function() {
+        snowList.push({x: Math.random() * snowfallCanvas.width, y: -10, size: randBetween(2, 8), yVel: randBetween(0.7, 1), xVel: randBetween(-0.1, 0.1)})
+
         spinSpeed = Math.min(spinSpeed + 1, 5);
         frost += 1;
         $("#frost").find("span").html(frost);
 
         localStorage.setItem("frost", frost);
 
-        const snow = $("<div>").addClass("snow");
+        /*const snow = $("<div>").addClass("snow");
         snow.css("left", `${Math.floor(Math.random() * 100)}%`);
 
         var startLeft = Math.random() * 100;
@@ -41,7 +48,7 @@ $( document ).ready(function() {
             snow.remove()
         }, duration)
 
-        $(".snowfall").append(snow);
+        $(".snowfall").append(snow);*/
     })
     setInterval(function() {
         spin =( spin + spinSpeed ) % 360;
@@ -49,9 +56,57 @@ $( document ).ready(function() {
         snowflake.find('svg').css('rotate', `${spin}deg`)
     }, 10)
 
+    snowfallCanvas.width = window.innerWidth;
+    snowfallCanvas.height = window.innerHeight;
+
+    $( window ).on( "resize", function() {
+        snowfallCanvas.width = window.innerWidth;
+        snowfallCanvas.height = window.innerHeight;
+    } );
+
     houseSmoke();
     loadStars();
+    store();
+    initCanvas();
 });
+
+let snowList = []
+
+function initCanvas() {
+    window.requestAnimationFrame(drawCanvas);
+}
+function drawCanvas(){
+    const ctx = document.getElementById("snowfall").getContext("2d");
+    ctx.globalCompositeOperation = "destination-over";
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clear canvas
+
+    let remaining = [];
+    for (let i = 0; i < snowList.length; i++) {
+        const snow = snowList[i];
+        
+        ctx.beginPath();
+        ctx.arc(snow.x, snow.y, snow.size, 0, 2 * Math.PI);
+        ctx.fillStyle = 'white';
+        ctx.fill();
+
+        snow.y += snow.yVel;
+        snow.x += snow.xVel;
+        if (snow.y < ctx.canvas.height) remaining.push(snow)
+    }
+    snowList = remaining;
+    window.requestAnimationFrame(drawCanvas);
+ }
+function store() {
+    const store = $(".store")
+    const upgrades = store.find(".upgrade");
+    upgrades.each(function (index) {
+        autoClickers[index] ??= 0;
+        $(this).on("click", function () {
+            autoClickers[index]++;
+            console.log(autoClickers)
+        });
+    });    
+}
 
 function loadStars() {
     const stars = $(".stars")
