a598161da1f1d3180bbe5bbcfe97e1ddfd262069
Alex Barker <alex.barker234@gmail.com>
2024-06-07 09:38:23 +0800

    Skills sectionv 2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 $(window).on("load", function () {
-    handleMeteors()
-    handleButtons()
-    handleFadeIn()
+    handleMeteors();
+    handleButtons();
+    handleFadeIn();
+
+    loadSkills();
 });
 
 const handleMeteors = () => {
@@ -35,7 +37,7 @@ const handleMeteors = () => {
         }, 600);
         meteorShower.append(meteor);
     }, 200);
-} 
+};
 const handleButtons = () => {
     const projectButtons = $(".selector");
     const projectsDiv = $(".projects");
@@ -44,52 +46,133 @@ const handleButtons = () => {
     projectButtons.each(function (index) {
         $(this).on("click", function () {
             const id = $(this).attr("id");
-            projectsDiv.addClass('fade')
-            setTimeout(() => projectsDiv.removeClass('fade'), animLength)
+            projectsDiv.addClass("fade");
+            setTimeout(() => projectsDiv.removeClass("fade"), animLength);
             setTimeout(() => {
                 // show all
-            if (!id) {
-                
-                projects.each(function () {
-                    $(this).removeClass("disabled");
-                });
-            }
-            // show as specified by id
-            else {
-                const selected = id.substring(1, id.length);
-                projects.each(function () {
-                    const project = $(this);
-                    if (project.hasClass(selected)) project.removeClass("disabled");
-                    else project.addClass("disabled");
-                });
-            }
-            }, animLength / 2)
+                if (!id) {
+                    projects.each(function () {
+                        $(this).removeClass("disabled");
+                    });
+                }
+                // show as specified by id
+                else {
+                    const selected = id.substring(1, id.length);
+                    projects.each(function () {
+                        const project = $(this);
+                        if (project.hasClass(selected)) project.removeClass("disabled");
+                        else project.addClass("disabled");
+                    });
+                }
+            }, animLength / 2);
         });
     });
-}
+};
 const handleFadeIn = () => {
     /** Fade in when bottom of screen is half down */
-    function isElementInView(elem) {      
+    function isElementInView(elem) {
         var elementCentre = $(elem).offset().top + $(elem).outerHeight();
         var windowBottom = $(window).scrollTop() + $(window).height();
 
         // MOBILE - fade in appearing at top
         if ($(window).width() < 600) {
-            return windowBottom > $(elem).offset().top + 200
+            return windowBottom > $(elem).offset().top + 200;
         }
 
-        return windowBottom > (elementCentre - elem.offsetHeight / 2)
+        return windowBottom > elementCentre - elem.offsetHeight / 2;
     }
 
-    const sections = $('.section');
+    const sections = $(".section");
     function handleScroll() {
         sections.each(function () {
             if (isElementInView(this)) {
-                $(this).addClass("shown")
+                $(this).addClass("shown");
             }
         });
     }
 
     handleScroll();
-    $(window).on('scroll', handleScroll);
-}
+    $(window).on("scroll", handleScroll);
+};
+
+const skills = [
+    {
+        id: "C#",
+        icon: "devicon-csharp-plain",
+        title: "C# & ASP.NET",
+    },
+    {
+        id: "Python",
+        icon: "devicon-python-plain",
+        title: "Python",
+    },
+    {
+        id: "CSS",
+        icon: "devicon-css3-plain",
+        title: "CSS",
+    },
+    {
+        id: "Sass",
+        icon: "devicon-sass-original",
+        title: "Sass",
+    },
+    {
+        id: "TailwindCSS",
+        icon: "devicon-tailwindcss-original",
+        title: "Tailwind CSS",
+    },
+    {
+        id: "HTML",
+        icon: "devicon-html5-plain",
+        title: "HTML",
+    },
+    {
+        id: "JavaScript",
+        icon: "devicon-javascript-plain",
+        title: "JavaScript",
+    },
+    {
+        id: "TypeScript",
+        icon: "devicon-typescript-plain",
+        title: "TypeScript",
+    },
+    {
+        id: "React",
+        icon: "devicon-react-original",
+        title: "React",
+    },
+    {
+        id: "Next.js",
+        icon: "devicon-nextjs-original-wordmark",
+        title: "Next.js",
+    },
+    {
+        id: "Jira",
+        icon: "devicon-jira-plain",
+        title: "Jira",
+    },
+];
+
+const loadSkills = () => {
+    const skillsContainer = document.getElementById("skills-container");
+
+    skills.forEach((skill) => {
+        const skillDiv = document.createElement("div");
+        skillDiv.classList.add("skill");
+
+        const iconDiv = document.createElement("div");
+        iconDiv.classList.add("icon");
+        const iconElement = document.createElement("i");
+        iconElement.classList.add(skill.icon);
+        iconDiv.appendChild(iconElement);
+
+        const titleDiv = document.createElement("div");
+        titleDiv.classList.add("title");
+        titleDiv.textContent = skill.title;
+
+        skillDiv.appendChild(iconDiv);
+        skillDiv.appendChild(titleDiv);
+
+        skillsContainer.appendChild(skillDiv);
+    });
+};
