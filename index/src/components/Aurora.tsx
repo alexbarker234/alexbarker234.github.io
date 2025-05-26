213750144ed7303d4b895e16a12f162a24ce4e0c
Alex Barker <alex.barker234@gmail.com>
2025-05-27 00:08:07 +0800

    Aurora

---
+++ index/src/components/Aurora.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useRef } from "react";
+
+// https://segmentfault.com/a/1190000041166007/en
+
+interface AuroraBorealisProps {
+  width?: number;
+  height?: number;
+  color1?: string;
+  color2?: string;
+  startFrames?: number;
+  id?: string;
+}
+
+export default function AuroraBorealis({
+  color1 = "#bd63c1",
+  color2 = "#53e5a6",
+  startFrames = 0,
+  id = "aurora-wave"
+}: AuroraBorealisProps) {
+  const filterRef = useRef<SVGFETurbulenceElement>(null);
+
+  const width = 500;
+  const height = 700;
+
+  useEffect(() => {
+    let frames = startFrames;
+    const rad = Math.PI / 180;
+
+    function animate() {
+      let bfx = 0.005;
+      let bfy = 0.005;
+      frames += 0.5;
+      bfx += 0.0025 * Math.cos(frames * rad);
+      bfy += 0.0025 * Math.sin(frames * rad);
+
+      const bf = `${bfx} ${bfy}`;
+      if (filterRef.current) {
+        filterRef.current.setAttribute("baseFrequency", bf);
+      }
+      requestAnimationFrame(animate);
+    }
+
+    requestAnimationFrame(animate);
+  }, []);
+
+  return (
+    <div className="relative blur-lg" style={{ width, height }}>
+      {/* Aurora effect */}
+      <div
+        className="absolute origin-center rotate-[38deg] scale-x-[1.4]"
+        style={{
+          width: width * 1.14,
+          height: height * 0.6,
+          background: `radial-gradient(circle at 100% 100%, transparent 45%, ${color1} 55%, ${color2} 65%, transparent 85%)`,
+          filter: `url(#${id}) blur(8px)`
+        }}
+      />
+
+      {/* SVG Filter */}
+      <svg>
+        <defs>
+          <filter id={id}>
+            <feTurbulence
+              ref={filterRef}
+              id="turbulence"
+              baseFrequency="0.003 0.003"
+              numOctaves="3"
+              seed="10"
+            />
+            <feDisplacementMap in="SourceGraphic" scale="72" />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+}
--- index/src/sections/Header.tsx
+++ index/src/sections/Header.tsx
@@ -1,3 +1,4 @@
+import AuroraBorealis from "@/components/Aurora";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import FadeInImage from "../components/FadeInImage";
 import MeteorShower from "../components/MeteorShower";
@@ -33,6 +34,7 @@ const generateStars = (numStars: number) => {
 export default function Header() {
   return (
     <header className="overflow-hidden relative w-full min-h-[400px] h-[30vw]">
+      {/* Stars and Shower Background */}
       <div
         className="absolute bg-night-sky w-[3000px] h-[3000px] rounded-full bottom-0 left-1/2
           transform -translate-x-1/2 z-[-1] overflow-hidden"
@@ -44,7 +46,26 @@ export default function Header() {
           {generateStars(50)}
           <MeteorShower />
         </div>
+
+        {/* Aurora */}
+        <div
+          className="w-screen h-[30vw] min-h-[400px] absolute bottom-30 left-1/2 transform
+            -translate-x-1/2 opacity-80 scale-x-[1.2]"
+        >
+          <div className="absolute left-1/5">
+            <AuroraBorealis />
+          </div>
+          <div className="absolute left-1/2 scale-x-[-1] rotate-12">
+            <AuroraBorealis
+              color1="#5538d6"
+              color2="#41d9c2"
+              startFrames={100}
+              id="aurora-wave-2"
+            />
+          </div>
+        </div>
       </div>
+      {/* Header Info */}
       <div
         className="main-title font-thin text-center absolute w-full top-[60%] transform
           -translate-y-1/2 text-[max(10vw,4rem)] drop-shadow-lg"
