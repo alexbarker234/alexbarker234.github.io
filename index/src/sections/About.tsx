f089cbe11056790a08bcf104937e9e18bfd80379
Alex Barker <alex.barker234@gmail.com>
2026-05-20 18:12:21 +0800

    music bit

--- index/src/sections/About.tsx
+++ index/src/sections/About.tsx
@@ -7,8 +7,11 @@ export default function About() {
       <HeaderText level="h1" className="text-4xl">
         About Me
       </HeaderText>
-      <div className="rounded-lg border border-white/10 bg-bg-dark/50 p-8 my-8 max-w-2xl mx-auto">
-        <p className="mb-4">
+      <div
+        className="rounded-lg border border-white/10 bg-bg-dark/50 p-8 my-8 max-w-2xl mx-auto flex
+          flex-col gap-4"
+      >
+        <p>
           I'm a <strong>software developer</strong> with a focus on{" "}
           <strong>full-stack web development</strong>. With a skill set spanning
           front-end and back-end technologies, I bring a creative,
@@ -21,6 +24,13 @@ export default function About() {
           integrate into my projects to create dynamic and engaging
           applications.
         </p>
+        <p>
+          I am also <strong>really</strong> into music, check out what I've been
+          grooving to lately:
+        </p>
+        <div className="flex flex-col gap-2 text-center py-4">
+          <span className="text-lg text-white/60 italic">Coming soon...</span>
+        </div>
       </div>
     </RevealingSection>
   );
